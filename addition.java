0e1bef1b1f554bfcf45d86e335e9020e0bf3271e
Sujal Sanjay Ghorpade <sujalghorpade1516@gmail.com>
2025-11-17 13:07:25 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class addition{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter Number 1: ");
+        int num1=sc.nextInt();
+        System.out.print("Enter Number 2: ");
+        int num2=sc.nextInt();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+https://chatgpt.com/c/691ac5b5-2788-8323-93a3-3799df1f6d52
+
+in addition and additiontest .java there are too many errorrs if possible take from user 
+
+mvn compile      # compiles Addition.java
+mvn test         # runs AdditionTest.java
+mvn package      # builds .jar file
